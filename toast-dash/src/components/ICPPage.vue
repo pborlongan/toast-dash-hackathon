9589cac1a03ef6e91c5cf8b7548983e85fdfd9ca
Patricia Borlongan <borlonganpatricia@gmail.com>
2024-11-21 00:08:40 -0700

    Added API Calls

--- toast-dash/src/components/ICPPage.vue
+++ toast-dash/src/components/ICPPage.vue
@@ -8,7 +8,7 @@
     <div class="w-100 mt-10">
       <v-text-field color="primary" label="Which job role are you hiring for?" variant="outlined" v-model="jobSearchText">
         <template #append>
-          <v-btn class="mb-1 mr-1" outlined color="indigo" @click="displayChat" :disabled="searchButton"> Search </v-btn>
+          <v-btn class="mb-1 mr-1" outlined color="indigo" @click="submitJobTitle" :disabled="searchButton"> Search </v-btn>
           <v-btn class="mb-1" outlined color="red" @click="clearChat" :disabled="newSearchButton"> Clear Search </v-btn>
         </template>
       </v-text-field>
@@ -30,15 +30,14 @@
                 <p>Hello! In order to build your Ideal Candidate Profile, we need you to answer a few questions related to the role.
                 </p>
                 <br />
-                <p>To start, which seniority level are you looking for for this role?</p>
+                <p>{{ store.jobTools.data.data.question }}</p>
               </v-card-text>
               <v-card-actions>
-                <v-btn variant="tonal" @click="addSeniority">Entry level</v-btn>
-                <v-btn variant="tonal" @click="addSeniority">Junior</v-btn>
-                <v-btn variant="tonal" @click="addSeniority">Intermediate</v-btn>
-                <v-btn variant="tonal" @click="addSeniority">Senior</v-btn>
-                <v-btn variant="tonal" @click="addSeniority">Manager</v-btn>
-                <v-btn variant="tonal" @click="addSeniority" >Executive</v-btn>
+                <div class="d-flex flex-wrap ga-3">
+                  <v-btn variant="tonal" v-for="(item) in store.jobTools.data.data.suggested_tools" @click="addJobTools">
+                    {{ item.name }}
+                  </v-btn>
+                </div>
               </v-card-actions>
             </v-card>
           </div>
@@ -60,7 +59,6 @@
           </div>
         </div>
 
-
         <div class="chat-bot pa-4" v-if="response1">
           <div class="assistant">
             <v-avatar color="info" size="x-large">
@@ -72,17 +70,15 @@
               variant="tonal"
             >
               <v-card-text>
-                <p>Amazing! According to our source, here are the technologies required for {{ jobSearchText }}.</p>
-                <br />
-                <p>
-                  Backend: PHP, Node.js, AWS <br/>
-                  Frontend: React, Javascript
-                </p>
-                <br />
-                <p class="mb-4">
-                  Is this correct?
-                </p>
+                <p>Select the top desirable skills for this role.</p>
               </v-card-text>
+              <v-card-actions>
+                <div class="d-flex flex-wrap ga-3">
+                  <v-btn variant="tonal" v-for="(item) in store.topSkills" @click="addDesirableSkills">
+                    {{ item }}
+                  </v-btn>
+                </div>
+              </v-card-actions>
             </v-card>
           </div>
         </div>
@@ -103,6 +99,7 @@
           </div>
         </div>
 
+
         <div class="chat-bot pa-4" v-if="response2">
           <div class="assistant">
             <v-avatar color="info" size="x-large">
@@ -114,22 +111,14 @@
               variant="tonal"
             >
               <v-card-text>
-
-                <p>Got it. According to our Source here are some similar requirements for the {{ jobSearchText }} Role.</p>
-                <br />
-                <p>
-                  Backend: C# .NET, mySQL <br />
-                  Frontend: React, Javascript
-                </p>
-                <br />
-                <p>
-                  Is this correct?
-                </p>
+                <p>{{ store.topSkillsResponse.data.data.question }}</p>
               </v-card-text>
-
               <v-card-actions>
-                <v-btn variant="tonal" @click="yesNo">Yes</v-btn>
-                <v-btn variant="tonal" @click="yesNo">No</v-btn>
+                <div class="d-flex flex-wrap ga-3">
+                  <v-btn variant="tonal" v-for="(item) in store.topSkillsResponse.data.data.undesirableSkills" @click="addUndesirableSkills">
+                    {{ item }}
+                  </v-btn>
+                </div>
               </v-card-actions>
             </v-card>
           </div>
@@ -151,7 +140,73 @@
           </div>
         </div>
 
+        <div class="chat-bot pa-4" v-if="response3">
+          <div class="assistant">
+            <v-avatar color="info" size="x-large">
+              <v-icon icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+            <v-card
+              title="TalentTua AI Assistant"
+              subtitle="New"
+              variant="tonal"
+            >
+              <v-card-text>
+                <p>Thank you so much! We're creating an ideal candidate profile for you:</p>
+                <br>
+
+                <div class="mb-4">
+                  <p class="text-sm-h6"> Here is the Ideal Candidate profile for a <span style="text-transform: capitalize; font-weight: bold;"> {{ store.ICPProfile.job_title }}</span> </p>
+                  <br>
+                  <p> <b>Job / Tools:</b> <span v-for="(item, index) in store.ICPProfile.jobToolsSkills">{{ index == store.ICPProfile.desirableSkills.length - 1 ? item : item.toString().concat(', ') }}</span> </p>
+                  <p> <b>Desirable Soft Skills: </b> <span v-for="(item, index) in store.ICPProfile.desirableSkills">{{ index == store.ICPProfile.desirableSkills.length - 1 ? item : item.toString().concat(', ') }}</span> </p>
+                  <p> <b>Undesirable Soft Skills: </b> <span v-for="(item, index) in store.ICPProfile.undesirableSkills">{{ index == store.ICPProfile.undesirableSkills.length - 1 ? item : item.toString().concat(', ') }}</span> </p>
+                </div>
+
+                .
+                .
+                .
+
+                <p class="mb-4">Please provide an email address so we can email the details to you!</p>
+              </v-card-text>
+            </v-card>
+          </div>
+        </div>
+
+        <div class="chat-bot pa-4 d-flex" v-if="response4">
+          <div class="response">
+            <v-card
+              title="You"
+              subtitle="New"
+              variant="tonal"
+            >
+              <v-card-text>
+                <p>
+                  {{ response4 }}
+                </p>
+              </v-card-text>
+            </v-card>
+          </div>
+        </div>
+
+
+        <div class="chat-bot pa-4" v-if="response4">
+          <div class="assistant">
+            <v-avatar color="info" size="x-large">
+              <v-icon icon="mdi-account-circle"></v-icon>
+            </v-avatar>
+            <v-card
+              title="TalentTua AI Assistant"
+              subtitle="New"
+              variant="tonal"
+            >
+              <v-card-text>
+                <p class="mb-2">Thank you so much! We will be sending it to you shortly.</p>
+              </v-card-text>
+            </v-card>
+          </div>
+        </div>
       </div>
+
       <div class="chat-bot-enter">
         <v-text-field class="chat-bot-input" v-model="inputText">
           <template #append>
@@ -164,15 +219,24 @@
 </template>
 
 <script setup lang="ts">
+  import {useAppStore} from "@/stores/app";
+  const store = useAppStore();
+
   const jobSearchText = ref('');
   const chatContainer = ref(false);
   const searchButton = ref(false);
   const newSearchButton = ref(true);
   const inputText = ref('');
   const inputChat = ref(true);
+
+  const jobSkills = ref([] as any);
+  const desirableSkills = ref([] as any);
+  const undesirableSkills = ref([] as any);
+
   const response1 = ref('');
   const response2 = ref('');
   const response3 = ref('');
+  const response4 = ref('');
 
   const displayChat = () => {
     chatContainer.value = !chatContainer.value;
@@ -191,38 +255,56 @@
     newSearchButton.value = false;
   }
 
-  const addSeniority = (event: Event) => {
+  const addJobTools = (event: Event) => {
     const element = event.currentTarget as Element;
-    inputText.value = element.textContent || '';
+    jobSkills.value.push(element.textContent || '');
+    inputText.value = jobSkills.value.join(", ");
+    inputChat.value = false;
+  }
+
+  const addDesirableSkills = (event: Event) => {
+    const element = event.currentTarget as Element;
+    desirableSkills.value.push(element.textContent || '');
+    inputText.value = desirableSkills.value.join(", ");
+    inputChat.value = false;
+  }
+
+  const addUndesirableSkills = (event: Event) => {
+    const element = event.currentTarget as Element;
+    undesirableSkills.value.push(element.textContent || '');
+    inputText.value = undesirableSkills.value.join(", ");
     inputChat.value = false;
   }
 
   const enterInput = () => {
-    if(response1.value == ''){
-      response1.value = inputText.value;
-    }else if(response2.value == '' && response1.value != inputText.value){
-      response2.value = inputText.value;
-    }else if(response3.value == '' && response2.value != inputText.value){
-      response3.value = inputText.value;
+    if(jobSkills.value.length > 0 && response1.value == ''){
+      response1.value = jobSkills.value.join(", ");
+      store.addSkills(jobSkills.value);
+    }else if(desirableSkills.value.length > 0 && response2.value == ''){
+      response2.value = desirableSkills.value.join(", ");
+      store.addDesirableSkills(desirableSkills.value);
+    }else if(undesirableSkills.value.length > 0 && response3.value == ''){
+      response3.value = undesirableSkills.value.join(", ");
+      store.addUndesirableSkills(undesirableSkills.value);
+    }else if(response1.value && response2.value && response3.value && response4.value == ''){
+      response4.value = inputText.value;
     }
 
     inputText.value = '';
     inputChat.value = true;
   }
 
-  const yesNo = (event: Event) => {
-    const element = event.currentTarget as Element;
-    inputText.value = element.textContent || '';
-    inputChat.value = false;
-  }
-
   watch(inputText, () => {
     if(inputText.value != '') {
       inputChat.value = false;
     }
-
-
   })
+
+
+  const submitJobTitle = () => {
+    store.sendJobTitle(jobSearchText.value);
+    chatContainer.value = !chatContainer.value;
+  }
 
 </script>
 
@@ -232,9 +314,7 @@
  }
 
  .chat {
-   height: 730px;
-   background-color: #E8EDF5;
-   flex: 1;
+   height: 500px;
    border-radius: 5px;
    position: relative;
 
@@ -246,8 +326,10 @@
    }
 
    .conversation-container {
+     border: none;
      overflow: scroll;
      height: 400px;
+     background-color: #E8EDF5;
    }
 
    .response {
--- toast-dash/src/stores/app.ts
+++ toast-dash/src/stores/app.ts
@@ -1,8 +1,97 @@
 // Utilities
 import { defineStore } from 'pinia'
+import axios from "axios";
 
 export const useAppStore = defineStore('app', {
   state: () => ({
     //
+    jobTools: {
+      data: {
+        data: {
+          question: '',
+          suggested_tools: [],
+        }
+      }
+    },
+    topSkillsResponse: {
+      data: {
+        data: {
+          undesirableSkills: [],
+          message: '',
+          question: '',
+        }
+      }
+    },
+    topSkills: [
+      "Ambitious Benevolence",
+      "Analytical",
+      "Analyzes Pitfalls",
+      "Animals",
+      "Artistic",
+      "Assertive",
+      "Authoritarian",
+      "Authoritative",
+      "Authoritative Collaboration",
+      "Benevolent Sacrifice",
+    ],
+    ICPProfile: {
+      user_id: '',
+      job_title: '',
+      jobToolsSkills: [] as any,
+      desirableSkills: [] as any,
+      undesirableSkills: [] as any
+    }
   }),
+  actions: {
+    async sendJobTitle(jobTitle: any){
+      this.ICPProfile.user_id = '12345';
+      this.ICPProfile.job_title = jobTitle;
+
+      try {
+        this.jobTools = await axios.post('https://talent-tua-api-endpoints.vercel.app/api/jobTitle', {
+          "user_id": "12345",
+          "job_title": jobTitle
+        })
+      }
+      catch (error) {
+        console.log(error)
+      }
+    },
+    addSkills(skills: []) {
+      if(this.ICPProfile.jobToolsSkills.length == 0) {
+        this.ICPProfile.jobToolsSkills = skills;
+      }else {
+        this.ICPProfile.jobToolsSkills = [this.ICPProfile.jobToolsSkills, ...skills]
+      }
+    },
+    async addDesirableSkills(skills: []){
+      try {
+        this.topSkillsResponse = await axios.post('\n' +
+          'https://talent-tua-api-endpoints.vercel.app/api/topSkills', {
+          "user_id": this.ICPProfile.user_id,
+          "topSkills": this.topSkills
+        })
+
+        if(this.ICPProfile.desirableSkills.length == 0) {
+          this.ICPProfile.desirableSkills = skills;
+        }else {
+          this.ICPProfile.desirableSkills = [this.ICPProfile.desirableSkills, ...skills]
+        }
+      }
+      catch (error) {
+        console.log(error)
+      }
+    },
+    addUndesirableSkills(skills: []){
+      if(this.ICPProfile.undesirableSkills.length == 0) {
+        this.ICPProfile.undesirableSkills = skills;
+      }else {
+        this.ICPProfile.undesirableSkills = [this.ICPProfile.undesirableSkills, ...skills]
+      }
+
+      console.log(this.ICPProfile)
+    }
+
+  }
+
 })
